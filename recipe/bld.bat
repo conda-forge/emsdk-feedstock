202f808852a926f4cfc0fd43a8269c2b9c99e3d1
martinRenou <martin.renou@gmail.com>
2022-05-20 14:32:31 +0200

    Try fixing the Windows build

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,13 @@
 :: Construct %PREFIX%\bin\emsdk
-mkdir %PREFIX%\bin
-echo "#!%PREFIX%/bin/python" >> %PREFIX%\bin\emsdk
-echo "# -*- coding: utf-8 -*-" >> %PREFIX%\bin\emsdk
-echo "import sys" >> %PREFIX%\bin\emsdk
-echo "from emsdk.emsdk import main" >> %PREFIX%\bin\emsdk
-echo "main(sys.argv[1:])" >> %PREFIX%\bin\emsdk
+if not exist "%PREFIX%\bin" mkdir %PREFIX%\bin
+echo "# -*- coding: utf-8 -*-" >> %PREFIX%\bin\emsdk.py
+echo "import sys" >> %PREFIX%\bin\emsdk.py
+echo "from emsdk.emsdk import main" >> %PREFIX%\bin\emsdk.py
+echo "main(sys.argv[1:])" >> %PREFIX%\bin\emsdk.py
+if errorlevel 1 exit 1
 
 :: Construct the emsdk Python module
 mkdir %PREFIX%\lib\site-packages\emsdk
 xcopy * %PREFIX%\lib\site-packages\emsdk /E
 type nul > %PREFIX%\lib\site-packages\emsdk\__init__.py
+if errorlevel 1 exit 1
